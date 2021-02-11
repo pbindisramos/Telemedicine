dcc5ad5f285f8b2c9372b0c3e6503f6d7d2553de
pbindisramos <p.bindisramos@gmail.com>
2021-02-11 18:36:16 -0300

    add login

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Head from "next/head";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>TELEMEDICINA APP SAA</title>
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="bg-gray-100 min-h-screen">
+        <div className="container mx-auto">
+          <main className="mt-20">{children}</main>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
