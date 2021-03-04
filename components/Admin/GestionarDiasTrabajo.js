f9b2f73fb14ce94d9b7cddad0dc870283c825c7b
pbindisramos <p.bindisramos@gmail.com>
2021-03-04 17:31:10 -0300

    horas medicas

--- components/Admin/GestionarDiasTrabajo.js
+++ components/Admin/GestionarDiasTrabajo.js
@@ -10,6 +10,15 @@ const dias = [
   'Domingo',
 ];
 
+let horas = [];
+const llenarHoras = () => {
+  for (let i = 0; i < 11; i++) {
+    horas += i;
+  }
+};
+
+llenarHoras();
+console.log(horas);
 const GestionarDiasTrabajo = () => {
   return (
     <div className='overflow-x-auto'>
@@ -25,44 +34,59 @@ const GestionarDiasTrabajo = () => {
                   <th className='py-3 px-6 text-center'>Turno Tarde</th>
                 </tr>
               </thead>
+
               <tbody className='text-gray-600 text-sm font-light'>
-                <tr className='border-b border-gray-200 hover:bg-gray-100'>
-                  <td className='py-3 px-6 text-left whitespace-nowrap'>
-                    <div className='flex items-center'>
-                      <span>Lunes</span>
-                    </div>
-                  </td>
-                  <td className='py-3 px-6 text-left'>
-                    <div className='flex items-center'>
-                      <div className='mr-2'>
-                        <div className='relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in'>
-                          <input
-                            type='checkbox'
-                            name='toggle'
-                            id='toggle'
-                            className='toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer'
-                          />
+                {dias.map((dia) => (
+                  <tr className='border-b border-gray-200 hover:bg-gray-100'>
+                    <td className='py-3 px-6 text-left whitespace-nowrap'>
+                      <div className='flex items-center'>
+                        <span>{`${dia}`}</span>
+                      </div>
+                    </td>
+                    <td className='py-3 px-6 text-left'>
+                      <div className='flex items-center'>
+                        <div className='mr-2'>
+                          <div className='relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in'>
+                            <input
+                              type='checkbox'
+                              name='toggle'
+                              id='toggle'
+                              className='toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer'
+                            />
+                            <label
+                              htmlfor='toggle'
+                              className='toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer'
+                            ></label>
+                          </div>
                           <label
-                            for='toggle'
-                            className='toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer'
+                            htmlfor='toggle'
+                            className='text-xs text-gray-700'
                           ></label>
                         </div>
-                        <label
-                          for='toggle'
-                          className='text-xs text-gray-700'
-                        ></label>
                       </div>
-                    </div>
-                  </td>
-                  <td className='py-3 px-6 text-center'>
-                    <div className='flex items-center justify-center'></div>
-                  </td>
-                  <td className='py-3 px-6 text-center'>
-                    <span className='bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs'>
-                      Active
-                    </span>
-                  </td>
-                </tr>
+                    </td>
+                    <td className='py-3 px-6 text-center'>
+                      <div className='flex items-center justify-center'>
+                        <select type='time' className='form-control mr-3'>
+                          <option value=''>08:00 am</option>
+                        </select>
+                        <select type='time' className='form-control'>
+                          <option value=''>10:00 am</option>
+                        </select>
+                      </div>
+                    </td>
+                    <td className='py-3 px-6 text-center'>
+                      <div className='flex items-center justify-center'>
+                        <select type='time' className='form-control mr-3'>
+                          <option value=''>08:00 am</option>
+                        </select>
+                        <select type='time' className='form-control'>
+                          <option value=''>10:00 am</option>
+                        </select>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
